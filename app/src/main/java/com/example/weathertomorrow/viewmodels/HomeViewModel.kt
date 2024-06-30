fe34f37272491b103b1e2fb8bd7ac69838ec1e6f
MehrajLatifli <meracletifli@gmail.com>
2024-07-01 02:11:21 +0400

    GOD bless universes

--- app/src/main/java/com/example/weathertomorrow/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/weathertomorrow/viewmodels/HomeViewModel.kt
@@ -55,14 +55,14 @@ class HomeViewModel @Inject constructor(
 
 
                         val weatherEntity = itemResponse.toWeatherEntity()
-
+                        withContext(Dispatchers.IO) {
                             try {
                                 delay(150)
                                 repoEntity.addWeatherEntity(weatherEntity)
                             } catch (e: Exception) {
                                 Log.e("DatabaseError", "Error adding weather entity: ${e.message}")
                             }
-
+                        }
                     } else {
                         _error.value = "No weather found"
                         _weathers.value = emptyList()
@@ -75,36 +75,37 @@ class HomeViewModel @Inject constructor(
                     Log.e("APIFailed", _error.value.toString())
 
 
-
+                    withContext(Dispatchers.IO) {
                         try {
 
                             delay(150)
-                            _loading.value = true
+
                             val weatherEntities = repoEntity.getWeatherEntity()
 
 
-
+                            withContext(Dispatchers.Main) {
 
                                 delay(150)
-                                _loading.value = false
+
                                 if (weatherEntities.isNotEmpty()) {
                                     _weathers.value = weatherEntities.map { it.toWeatherResponse() }
                                 } else {
                                     _error.value = "No cached weather found"
                                     _weathers.value = emptyList()
                                 }
-
+                            }
                         } catch (e: Exception) {
                             Log.e("DatabaseError", "Error getting weather entities: ${e.message}")
 
 
                             withContext(Dispatchers.Main) {
+                                delay(150)
                                 _error.value = "Error getting cached data: ${e.message}"
                                 _weathers.value = emptyList()
                             }
                         }
                     }
-
+                }
             }
         }
     }
