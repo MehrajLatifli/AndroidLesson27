322c58ce52477651fdd6aaf721ba614e4b69d143
MehrajLatifli <meracletifli@gmail.com>
2024-07-02 00:54:39 +0400

    GOD bless universes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -148,6 +148,7 @@ dependencies {
 
     //Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
 
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
--- app/src/main/java/com/example/weathertomorrow/source/local/LocalDao.kt
+++ app/src/main/java/com/example/weathertomorrow/source/local/LocalDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.weathertomorrow.models.entities.WeatherEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface LocalDao {
@@ -12,6 +13,7 @@ interface LocalDao {
     fun add(weatherEntity: WeatherEntity)
 
     @Query("SELECT * FROM weathers")
-    suspend fun getweathers(): List<WeatherEntity>
+    fun getWeatherEntities(): Flow<List<WeatherEntity>>
+
 
 }
